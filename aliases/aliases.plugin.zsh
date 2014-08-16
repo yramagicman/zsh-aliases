f36b0affd2d8bd6ab61a17e4beb16fe07a35c80a
Jonathan <jonathandavis@gilsons.org>
2014-08-16 09:30:57 -0400

    modify update command

--- aliases/aliases.plugin.zsh
+++ aliases/aliases.plugin.zsh
@@ -249,7 +249,7 @@ if [[ $_myos == Darwin ]]; then
     # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm,
     # and their installed packages
     alias update='sudo -v; npm update npm -g; npm update; gem update; brew \
-    update; brew upgrade; viupdate; bell'
+    update; brew upgrade;  bell'
 else
     alias sync="yaourt -Sy; updatenotify; gup"
     alias update="updatelog; yaourt -Syua; bell"
