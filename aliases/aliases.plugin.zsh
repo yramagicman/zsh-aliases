c8d1a1fe9fe0df520274abc84d16e8210a5a2090
Jonathan <jonathandavis@gilsons.org>
2014-08-19 10:57:39 -0400

    fold markers, moving things around

--- aliases/aliases.plugin.zsh
+++ aliases/aliases.plugin.zsh
@@ -8,7 +8,6 @@ else
     gvim="mvim"
     kill="killall"
 fi
-
 #{{{> Easier navigation: .., ..., ...., ....., ~ and -
 alias ..="cd .."
 alias ...="cd ../.."
@@ -27,7 +26,6 @@ alias apps="cd /Applications"
 alias themes="cd "$projDir"/wordpress/wp-content/themes"
 alias gits="cd ~/Gits"
 #}}}
-
 #{{{ apps, vim, git and yo mamma
 alias g="git"
 alias py="python"
@@ -54,27 +52,22 @@ alias npm="npm"
 alias gem="sudo gem"
 alias _="cd "$projDir"/wordpress/wp-content/themes/_skeletheme/ && ls"
 #}}}
-# quick jump to files and directories
-#{{{
+#{{{ quick jump to files and directories
 alias _="cd "$projDir"/wordpress/wp-content/themes/_skeletheme/"
 alias a="v ~/.oh-my-zsh/custom/plugins/zsh-aliases/aliases/aliases.plugin.zsh"
 alias vimrc="v ~/.vim/config/"
 alias gvimrc="m ~/.gvimrc"
 #}}}
-# if git error
-#{{{
+#{{{ if git error
 alias gitfix="git config remote.origin.push HEAD"
 alias killgit="rm -rf .git && rm -rf .git*"
 #}}}
-# always recursive and verbose
-#{{{
+#{{{ always recursive and verbose
 alias cp="cp -rv"
 alias rm="rm -rv"
 alias mv='mv -v'
 #}}}
-# web dev stuff
-# somewhat mac specific, find alternatives
-#{{{
+#{{{ web dev stuff, somewhat mac specific, find alternatives
 alias mamp="open /Applications/MAMP/MAMP.app/Contents/MacOS/MAMP; exit"
 alias wp="open -a \"Google Chrome Canary\" http://localhost:8888/wordpress/ && \
 open -a \"Google Chrome Canary\" http://localhost:8888/wordpress/wp-admin/"
@@ -86,8 +79,7 @@ alias speedtest="wget -O /dev/null \
  http://speedtest.wdc01.softlayer.com/downloads/test10.zip"
  alias wget="wget -c --no-check-certificate"
 #}}}
-# colorize stuff, utility commands
-#{{{
+#{{{ colorize stuff, utility commands
 # Detect which `ls` flavor is in use
 # and other ls related commands
 #{{{
@@ -96,32 +88,24 @@ if ls --color > /dev/null 2>&1; then # GNU `ls`
 else # OS X `ls`
     colorflag="-G"
 fi
-
 # List all files colorized in long format
 alias l="ls -l ${colorflag}"
 alias ls-a="ls -a ${colorflag}"
 # List all files colorized in long format, including dot files
 alias la="ls -la ${colorflag}"
-
 # List only directories
 alias lsd='ls -l ${colorflag} | grep "^d"'
-
 # Always use color output for `ls`
 alias ls="command ls ${colorflag}"
 export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
-
 #}}}
-
 #web related
 # {{{
 # Gzip-enabled `curl`
 alias gurl="curl --compressed"
-
 # Enhanced WHOIS lookups
 alias whois="whois -h whois-servers.net"
-
 #}}}
-
 # IP addresses
 #{{{
 if [[ $_myos == Darwin ]]; then
@@ -137,35 +121,31 @@ if [[ $_myos == Linux ]]; then
     alias sniff="sudo tcpdump -s 0 -A -i wlp4s0 port 80"
     alias httpdump="sudo tcpdump -i wlp4s0 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 fi
-
 alias rip="dig +short myip.opendns.com @resolver1.opendns.com"
 if [[ $_myos == Darwin ]]; then
     alias localip="ipconfig getifaddr en1"
     alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
 fi
 #}}}
-
 if [[ $_myos == Darwin ]]; then
     # Flush Directory Service cache
     alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
 fi
 #}}}
-# random commands
-# {{{
+# {{{ random commands
 # Canonical hex dump; some systems have this symlinked
 command -v hd > /dev/null || alias hd="hexdump -C"
-
+#{{{ checksum on osx
 if [[ $_myos == Darwin ]]; then
     # OS X has no `md5sum`, so use `md5` as a fallback
     command -v md5sum > /dev/null || alias md5sum="md5"
-
     # OS X has no `sha1sum`, so use `shasum` as a fallback
     command -v sha1sum > /dev/null || alias sha1sum="shasum"
     # Trim new lines and copy to clipboard
     alias c="tr -d '\n' | pbcopy"
 fi
-# cleanup Stuff
-#{{{
+#}}}
+#{{{ cleanup Stuff
 if [[ $_myos == Darwin ]]; then
     # Clean up LaunchServices to remove duplicates in the “Open With” menu
     alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
@@ -174,40 +154,31 @@ alias sasscleanup="rm -rfv ./**/.sass-cache && rm -rfv ./.sass-cache"
 # Recursively delete `.DS_Store` files
 alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 #}}}
-# ROT13-encode text. Works for decoding, too! ;)
-alias rot13='tr a-zA-Z n-za-mN-ZA-M'
-
+#{{{ mac specific shortcuts for emptying trash and other various things
 if [[ $_myos == Darwin ]]; then
     # Empty the Trash on all mounted volumes and the main HDD
     # Also, clear Apple’s System Logs to improve shell startup speed
     alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
     # show and hide things
-    #{{{
     # Show/hide hidden files in Finder
     alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
     alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
-
     # Hide/show all desktop icons (useful when presenting)
     alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-    alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
-    #}}}
-fi
-# URL-encode strings
-alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
-
-if [[ $_myos == Darwin ]]; then
-    # Merge PDF files
+     # Merge PDF files
     # Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
     alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
-
     # Disable Spotlight
     alias spotoff="sudo mdutil -a -i off"
     # Enable Spotlight
     alias spoton="sudo mdutil -a -i on"
-
     # PlistBuddy alias, because sometimes `defaults` just doesn’t cut it
     alias plistbuddy="/usr/libexec/PlistBuddy"
+   alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
 fi
+#}}}
+# URL-encode strings
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
 # Ring the terminal bell, and put a badge on Terminal.app’s Dock icon
 # (useful when executing time-consuming commands)
 if [[ $_myos == Darwin ]]; then
@@ -218,20 +189,12 @@ else
 fi
 alias cat="cat"
 alias less="less -N"
-# Intuitive map function
-# For example, to list all directories that contain a certain file:
-# find . -name .gitattributes | map dirname
-alias map="xargs -n1"
-
 # One of @janmoesen’s ProTip™s
 for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
     alias "$method"="lwp-request -m '$method'"
 done
 #}}}
-
-# Stuff I never really use but cannot delete either because of
-# http://xkcd.com/530/
-#{{{
+#{{{ Stuff I never really use but cannot delete either because of http://xkcd.com/530/
 if [[ $_myos == Darwin ]]; then
     alias stfu="osascript -e 'set volume output muted true'"
     alias shutup="osascript -e 'set volume output muted true'"
@@ -239,7 +202,6 @@ if [[ $_myos == Darwin ]]; then
     alias headphones="osascript -e 'set volume 2'"
 fi
 #}}}
-
 alias starwars="telnet towel.blinkenlights.nl"
 if [[ $_myos == Darwin ]]; then
     alias lsblk='diskutil list'
@@ -259,8 +221,8 @@ fi
 #}}}
 if [[ $_myos == Darwin ]]; then
     alias eject="diskutil eject"
+    alias rmspot="sudo rm -rfv ~/Library/Caches/com.spotify.client/Storage/"
 fi
-alias rmspot="sudo rm -rfv ~/Library/Caches/com.spotify.client/Storage/"
 alias purge="sudo purge"
 alias compile="clang++ -std=c++11 -stdlib=libc++ -Weverything -w"
 # easy reload of zsh stuff
@@ -270,8 +232,6 @@ alias kill-grunt=$kill" grunt; rm ~/grunt.log"
 alias gup="source ~/.update.sh"
 alias cb="source ~/.vim/bundle/.commit.sh"
 if [[ $_myos == Linux ]]; then
-    alias dtswap="swap ~/.xinitrc ~/.xinitrc.other >> /dev/null; cat ~/.xinitrc \
-    | grep awesome"
     alias python="python2"
     alias cal="cal -3"
 fi
@@ -288,12 +248,11 @@ if [[ $_myos == Darwin ]]; then
     alias ql="qlmanage -p"
 else
     alias cmdtocb='history | tail -n 1 | awk '"'"'{for(i=2;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'"'"' | clipboard'
-    alias open='xdg-open'
     alias poweroff="sudo umount /mnt/share; sudo shutdown -h now"
     alias reboot="sudo umount /mnt/share; sudo shutdown -r now"
     alias ql="xdg-open"
 fi
-alias q="exit;exit"
+alias q="exit"
 alias tkill="tmux kill-session -t"
 alias pwgen="pwgen -c -n -s -y 16 -1"
 alias count="ls -l1 | wc -l"
