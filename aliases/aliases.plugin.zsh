8d7c67920da7b87d5e89b457d9f1a5ab1da39ee8
Jonathan <jonathandavis@gilsons.org>
2015-01-19 11:10:18 -0500

    That wouldn't have worked anyway

--- aliases/aliases.plugin.zsh
+++ aliases/aliases.plugin.zsh
@@ -197,7 +197,7 @@ alias starwars="telnet towel.blinkenlights.nl"
 if [[ $_myos == Darwin ]]; then
     # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm,
     # and their installed packages
-    alias update='git checkout 0c77f5; npm install -g npm@latest;  npm update -g; \
+    alias update='npm install -g npm@latest;  npm update -g; \
     gem update; brew update; brew upgrade; brew linkapps; git checkout master;  bell'
 else
     alias sync="yaourt -Sy; un; gup"
