a25cb1db8aefbe6c41d735411ea52eb7338acba3
Jonathan <jonathandavis@gilsons.org>
2018-04-27 08:56:43 -0400

    autoload all the things

--- init.zsh
+++ init.zsh
@@ -1,3 +1,7 @@
 fpath=("${0:h}" $fpath)
-source "${0:h}/functions/functions.plugin.zsh"
+fpath=("${0:h}/functions" $fpath)
+# source "${0:h}/functions/functions.plugin.zsh"
+for f in ${0:h}/functions/*; do
+    autoload "$(basename $f )"
+done
 source "${0:h}/aliases/aliases.plugin.zsh"
