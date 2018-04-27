c70ad3b77d917f4365862350756b46a24bdd2da3
Jonathan <jonathandavis@gilsons.org>
2018-04-27 14:14:38 -0400

    globbing is so much faster

--- init.zsh
+++ init.zsh
@@ -1,7 +1,5 @@
 fpath=("${0:h}" $fpath)
 fpath=("${0:h}/functions" $fpath)
 # source "${0:h}/functions/functions.plugin.zsh"
-for f in ${0:h}/functions/*; do
-    autoload "$(basename $f )"
-done
+autoload ${0:h}/functions/*
 source "${0:h}/aliases/aliases.plugin.zsh"
