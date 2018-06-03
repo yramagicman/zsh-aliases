a09a794c460620168cc11ab395cada7ffcaf5829
Jonathan <jonathandavis@gilsons.org>
2018-06-03 13:51:06 -0400

    update vimrc alias

--- aliases/aliases.plugin.zsh
+++ aliases/aliases.plugin.zsh
@@ -35,7 +35,7 @@ alias bc='bc -l'
 alias sudo='sudo '
 #}}}
 #{{{ quick jump to files and directories
-alias vimrc="builtin cd ~/.vim/pack/mine/; vim ."
+alias vimrc="vim ~/.vimrc"
 #}}}
 #{{{ always recursive and verbose
 alias cp="cp -rv"
